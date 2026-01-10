584dd1c0fa823c2a3c8c5e2d02c10ab9e1f3ba3b
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-01-10 22:30:40 +0545

    Fix semaphore functions and add missing newline

--- src/semaphore.c
+++ src/semaphore.c
@@ -6,8 +6,10 @@ void sem_init(semaphore *s, int value) {
 
 void sem_wait(semaphore *s) {
     int expected;
+    
     while (1) {
         expected = atomic_load(&s->count);
+        
         if (expected > 0) {
             if (atomic_compare_exchange_strong(&s->count, &expected, expected - 1)) {
                 break;
@@ -19,4 +21,3 @@ void sem_wait(semaphore *s) {
 void sem_signal(semaphore *s) {
     atomic_fetch_add(&s->count, 1);
 }
-
